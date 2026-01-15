f1d0dd8701e06913318297b8f1a7bf756278f2c1
Karmanya3183 <karmanyavishalacharya@gmail.com>
2026-01-15 14:26:57 +0530

    Update and rename question1.cpp to question1a.cpp

---
+++ Week1/question1a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int sumBetween(int first, int last) {
+    int sum = 0;
+    for (int i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main() {
+    int first, last;
+    cout << "Enter first number: ";
+    cin >> first;
+    cout << "Enter last number: ";
+    cin >> last;
+
+    cout << "Sum = " << sumBetween(first, last) << endl;
+    return 0;
+}
